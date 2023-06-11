e61863f59d6f777f6fe9d8c7889a2b94f4cb3c81
badguybandi <55437597+badguybandi@users.noreply.github.com>
2023-06-11 09:01:14 +0000

    Styled the pages for the jobs

--- database.py
+++ database.py
@@ -18,3 +18,16 @@ def load_jobs_from_db():
       jobs.append(dict(zip(result.keys(), row)))
 
     return jobs
+
+def load_job_from_db(id):
+  query = text(f"SELECT * FROM jobs WHERE id = {id}")
+  with engine.connect() as conn:
+    result = conn.execute(
+      query
+    )
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(zip(result.keys(), rows[0]))
+    
