b8fda2c9b2d14ef54fb3952f61da3c9f82cf4cf9
badguybandi <55437597+badguybandi@users.noreply.github.com>
2023-06-11 11:30:58 +0000

    Created an application table in database and stored to it

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db,load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -15,12 +15,26 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+
 @app.route("/job/<id>")
 def show_job(id):
   job = load_job_from_db(id)
   if not job:
     return "Not Found", 404
-  return render_template('jobpage.html',job=job)
+  return render_template('jobpage.html', job=job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+
+  add_application_to_db(id,data)
+  #store this in DB
+  #display acknowledgement
+  #send email
+  return render_template('application_submitted.html', application=data,job=job)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
